5155f1cd29881bac2a7ebcca0cb17b963685dc59
Aitor Guerrero <aguerrerolasarte@gmail.com>
2017-03-22 13:57:48 +0100

    index moved to root

---
+++ index.ts
@@ -0,0 +1,3 @@
+export * from './src/after.service';
+export * from './src/before.service';
+export * from './src/on-error.service';
--- src/index.ts
+++
@@ -1,3 +0,0 @@
-export * from './after.service';
-export * from './before.service';
-export * from './on-error.service';
